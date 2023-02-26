f56d5535a293c5d4cb77f9507746140d47bcebee
Erick Poppe <erickpoppe@hotmail.com>
2023-02-26 12:31:08 -0800

    Adding reactjs component linked to wasm lib

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import React, { useState } from 'react';
+import { add } from '../../pkg/reactjs_wasm.js';
 
 function App() {
+  const [result, setResult] = useState(0);
+
+  const handleClick = () => {
+    const a = 2;
+    const b = 3;
+    const res = add(a, b);
+    setResult(res);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React + WebAssembly Example</h1>
+      <button onClick={handleClick}>Add 2 + 3</button>
+      <p>Result: {result}</p>
     </div>
   );
 }
