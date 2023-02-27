9c841a71146bea3ca18e5602c460bcab268482e1
Erick Poppe <erickpoppe@hotmail.com>
2023-02-27 13:32:07 -0800

    Correct errors in App.tsx

--- react-wasm-tutorial/src/App.tsx
+++ react-wasm-tutorial/src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import and call
+import React, { useEffect, useState } from 'react';
 import init, { add } from "wasm-lib";
 import logo from './logo.svg';
 import './App.css';
